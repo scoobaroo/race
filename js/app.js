43f180c07a97ad9a8fb4f9e1380e51b784aa86ba
scoobaroo <scoobaroo415@gmail.com>
2016-03-25 23:54:48 -0700

    added race track, player objects, animation, and reset button

--- js/app.js
+++ js/app.js
@@ -1,23 +1,45 @@
 $(document).on("ready", function(){
 
-function makeCar(color,make,model,horsepower,torque,suspension){
+  function makeCar(color){
     this.color = color;
-    this.make = make;
-    this.mode = model;
-    this.horsepower = horsepower;
-    this.torque = torque;
-    this.suspension = suspension;
     this.drive = function drive(event){};
-    this.accelerate = function accelerate(event){};
-    this.deccelerate = function deccelerate(evene){};
-}
+    this.position = function position(){};
+  }
+  makeCar();
 
-var clock = {time: timeLeft};
+  function makeRaceTrack(length){
+    this.length = length;
+    this.resetRaceTrack = function resetRaceTrack(){
+    };
+  }
 
-function makeRaceTrack(length){
-  this.length = length;
-  this.resetRaceTrack = function resetRaceTrack(){
-  };
-}
+  // $("button").on("click", $("#player1").css('left',30));
+  if($("button").on("click", function(event){
+    console.log("reset clicked");
+    $("#player1").css('margin-left',10);
+    $("#player2").css('margin-left',10);
+  }));
 
+
+  function example_animate(px) {
+	$('#player1').animate({
+		'marginLeft' : px
+	});
+  }
+
+  $(document).keydown(function(e) {
+      if(e.which == 76) {
+          $('#player1').animate({
+              'marginLeft' : "+=200px"
+          });
+
+      }});
+  $(document).keydown(function(e) {
+      if(e.which == 65) {
+          $('#player2').animate({
+              'marginLeft' : "+=200px"
+          });
+      }});
 });
+
+  $("#player1").css('margin-left',1375);
